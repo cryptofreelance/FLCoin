72619d54f2e86229b8ebb1c05a3c91acb6fb0904
cryptofreelance <cryptofreelance@protonmail.com>
2018-06-21 10:28:28 +0300

    GetDGWPastBlocks

--- src/pow.cpp
+++ src/pow.cpp
@@ -83,7 +83,7 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const Conse
 unsigned int static DarkGravityWave(const CBlockIndex* pindexLast, const Consensus::Params& params) {
     /* current difficulty formula, flcoin - DarkGravity v3, written by Evan Duffield - evan@flcoin.org */
     const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
-    params.GetDGWPastBlocks(pindexLast->nHeight);
+    int64_t nPastBlocks = params.GetDGWPastBlocks(pindexLast->nHeight);
 
     // make sure we have at least (nPastBlocks + 1) blocks, otherwise just return powLimit
     if (!pindexLast || pindexLast->nHeight < nPastBlocks) {
